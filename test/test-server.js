4d6a89a4ad7ee6d05ba4d1097630030311a026b5
Peter Szujewski <pszujewski@gmail.com>
2017-03-19 18:26:04 -0400

    tests correctly updated

--- test/test-server.js
+++ test/test-server.js
@@ -13,6 +13,111 @@ const should = chai.should();
 // see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
+// Recipes
+
+describe('Recipes', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should list recipes on GET', function() {
+    // recall that we manually add some recipes to `Recipes`
+    // inside `recipesRouter.js`. Later in this course,
+    // once we're using a database layer, we'll seed
+    // our database with test data, and we can form our expectations
+    // about what GET should return, based on what we know about
+    // the state of our database.
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+
+        res.body.should.have.length.of.at.least(1);
+
+        // each item should be an object with key/value pairs
+        // for `id`, `name` and `ingredients`.
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys('id', 'name', 'ingredients');
+        });
+      });
+  });
+
+  it('should add a recipe on POST', function() {
+    const newRecipe = {
+        name: 'coffee', ingredients: ['ground coffee', 'hot water']};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newRecipe)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys('id', 'name', 'ingredients');
+        res.body.name.should.equal(newRecipe.name);
+        res.body.ingredients.should.be.a('array');
+        res.body.ingredients.should.include.members(newRecipe.ingredients);
+      });
+  });
+
+  it('should update recipes on PUT', function() {
+
+    const updateData = {
+      name: 'foo',
+      ingredients: ['bizz', 'bang']
+    };
+
+    return chai.request(app)
+      // first have to get recipes so have `id` for one we
+      // want to update. Note that once we're working with databases later
+      // in this course get the `id` of an existing instance from the database,
+      // which will allow us to isolate the PUT logic under test from our
+      // GET interface.
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData)
+      })
+      .then(function(res) {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.include.keys('id', 'name', 'ingredients');
+        res.body.name.should.equal(updateData.name);
+        res.body.id.should.equal(updateData.id);
+        res.body.ingredients.should.include.members(updateData.ingredients);
+      });
+  });
+
+  it('should delete recipes on DELETE', function() {
+    return chai.request(app)
+      // first have to get recipes so have `id` for one we want
+      // to delete. Note that once we're working with databases later
+      // in this course, we'll be able get the `id` of an existing instance
+      // directly from the database, which will allow us to isolate the DELETE
+      // logic under test from our GET interface
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`)
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+});
+
+// End Recipes Tests
 
 describe('Shopping List', function() {
 
@@ -21,18 +126,15 @@ describe('Shopping List', function() {
   // doing `return runServer`. If we didn't return a promise here,
   // there's a possibility of a race condition where our tests start
   // running before our server has started.
-  before(function() {
-    return runServer();
-  });
 
   // although we only have one test module at the moment, we'll
   // close our server at the end of these tests. Otherwise,
   // if we add another test module that also has a `before` block
   // that starts our server, it will cause an error because the
   // server would still be running from the previous tests.
-  after(function() {
-    return closeServer();
-  });
+  // after(function() {
+  //   return closeServer();
+  // });
 
   // test strategy:
   //   1. make request to `/shopping-list`
